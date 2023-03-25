5cbd378d931a340fa2c8ef7b45c3f6dec74ca333
danielstevenescobar <125221567+danielstevenescobar@users.noreply.github.com>
2023-03-25 15:43:09 -0300

    Add files via upload

---
+++ bucletipostring.java
@@ -0,0 +1,13 @@
+package com.ejerciciosTema3;
+
+public class bucletipostring {
+
+    public static void main(String[] args) {
+
+        String[] horasdeinicio = new String[] {"hora1", "hora2", "hora3"};
+
+        for(int i = 0; i < horasdeinicio.length; i++) {
+            System.out.println(horasdeinicio[i]);
+        }
+    }
+}
